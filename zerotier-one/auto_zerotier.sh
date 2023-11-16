9bf4ba0d8ef61c8f5b661afb41418c270b3051b7
MINGERTAI <104557080+MINGERTAI@users.noreply.github.com>
2023-11-16 15:50:21 +0800

    Update auto_zerotier.sh

--- zerotier-one/auto_zerotier.sh
+++ zerotier-one/auto_zerotier.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-# debain ubuntu自动安装zerotier 并设置的为planet服务器
+# debain ubuntu自动安装 zerotier 并设置的为planet服务器
 # addr服务器公网ip+port
  ip=`wget http://ipecho.net/plain -O - -q ; echo`
  addr=$ip/9993
@@ -7,7 +7,7 @@
  apt update -y
  apt install curl -y
  echo "********************************************************************************************************************"
- echo "**********deabin unbuntu自动安装zerotier 并设置的为planet服务器 放在root目录执行**********************************"
+ echo "***************deabin unbuntu自动安装zerotier 并设置的为planet服务器 放在root目录执行**********************************"
  curl -s https://install.zerotier.com/ | sudo bash
  
  identity=`cat /var/lib/zerotier-one/identity.public`
@@ -27,15 +27,17 @@ mv ./world.bin ./planet
 \cp -r ./planet /var/lib/zerotier-one/
 \cp -r ./planet /root
 systemctl restart zerotier-one.service
-wget https://gitee.com/opopop880/ztncui/attach_files/932632/download/ztncui_0.8.7_amd64.deb
+wget https://raw.githubusercontent.com/MINGERTAI/QTV/main/zerotier-one/ztncui_0.8.7_amd64.deb
 sudo dpkg -i ztncui_0.8.7_amd64.deb*
 cd /opt/key-networks/ztncui/
-echo "HTTPS_PORT = 3443" >>./.env
+sudo sh -c "echo HTTPS_PORT=3443 >> /opt/key-networks/ztncui/.env"
+sudo sh -c "echo NODE_ENV=production >> /opt/key-networks/ztncui/.env"
 secret=`cat /var/lib/zerotier-one/authtoken.secret`
-echo "ZT_TOKEN = $secret" >>./.env
-echo "ZT_ADDR=127.0.0.1:9993" >>./.env
-echo "NODE_ENV = production" >>./.env
-echo "HTTP_ALL_INTERFACES=yes" >>./.env
+sudo sh -c "echo ZT_TOKEN = $secret >> /opt/key-networks/ztncui/.env"
+sudo sh -c "echo ZT_ADDR=127.0.0.1:9993 >> /opt/key-networks/ztncui/.env"
+sudo sh -c "echo HTTP_ALL_INTERFACES=yes >> /opt/key-networks/ztncui/.env"
+sudo chmod 400 /opt/key-networks/ztncui/.env
+sudo chown ztncui.ztncui /opt/key-networks/ztncui/.env
 systemctl restart ztncui
 rm -rf /root/ZeroTierOne
 echo "**********安装成功*********************************************************************************"
