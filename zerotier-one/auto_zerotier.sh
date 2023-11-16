775741a24277de4faba82f040dc76b5e2ee8d37f
MINGERTAI <104557080+MINGERTAI@users.noreply.github.com>
2023-11-16 16:31:23 +0800

    Update auto_zerotier.sh

--- zerotier-one/auto_zerotier.sh
+++ zerotier-one/auto_zerotier.sh
@@ -1,34 +1,35 @@
 #!/bin/bash 
 # debain ubuntu自动安装 zerotier 并设置的为planet服务器
 # addr服务器公网ip+port
- ip=`wget http://ipecho.net/plain -O - -q ; echo`
- addr=$ip/9993
- apt autoremove
- apt update -y
- apt install curl -y
- echo "********************************************************************************************************************"
- echo "***************deabin unbuntu自动安装zerotier 并设置的为planet服务器 放在root目录执行**********************************"
- curl -s https://install.zerotier.com/ | sudo bash
+ip=`wget http://ipecho.net/plain -O - -q ; echo`
+addr=$ip/9993
+apt autoremove
+apt update -y
+apt install curl -y
+echo "********************************************************************************************************************"
+echo "#                deabin unbuntu自动安装zerotier 并设置的为planet服务器 放在root目录执行                                #"
+echo "********************************************************************************************************************"
+curl -s https://install.zerotier.com/ | sudo bash
  
- identity=`cat /var/lib/zerotier-one/identity.public`
- echo "identity :$identity=============================================="
- apt-get -y install build-essential
- apt-get install git -y
- git clone https://gitee.com/MINGERTAI/ZeroTierOne.git
- cd ./ZeroTierOne/attic/world/
+identity=`cat /var/lib/zerotier-one/identity.public`
+echo "identity :$identity=============================================="
+apt-get -y install build-essential
+apt-get install git -y
+git clone https://gitee.com/MINGERTAI/ZeroTierOne.git
+cd ./ZeroTierOne/attic/world/
 sed -i '/roots.push_back/d' ./mkworld.cpp
 sed -i '/roots.back()/d' ./mkworld.cpp 
 sed -i '85i roots.push_back(World::Root());' ./mkworld.cpp 
 sed -i '86i roots.back().identity = Identity(\"'"$identity"'\");' ./mkworld.cpp 
 sed -i '87i roots.back().stableEndpoints.push_back(InetAddress(\"'"$addr"'\"));' ./mkworld.cpp 
 source ./build.sh
-cd ZeroTierOne/attic/world/
+cd /root/ZeroTierOne/attic/world/
 ./mkworld
 mv ./world.bin ./planet
 \cp -r ./planet /var/lib/zerotier-one/
 \cp -r ./planet /root
 systemctl restart zerotier-one.service
-wget https://github.com/MINGERTAI/QTV/raw/main/zerotier-one/ztncui_0.8.6_amd64.deb > ./root/ztncui_0.8.6_amd64.deb
+cd && wget https://github.com/MINGERTAI/QTV/raw/main/zerotier-one/ztncui_0.8.6_amd64.deb
 sudo dpkg -i ztncui_0.8.7_amd64.deb
 cd && cd /opt/key-networks/ztncui/
 sudo sh -c "echo HTTPS_PORT=3443 >> /opt/key-networks/ztncui/.env"
