d0938557be85e91ba084d24496bc366069d72e0d
MINGERTAI <104557080+MINGERTAI@users.noreply.github.com>
2023-12-26 14:03:16 +0800

    Update auto-zerotier.sh

--- zerotier-planet/auto-zerotier.sh
+++ zerotier-planet/auto-zerotier.sh
@@ -2,8 +2,7 @@
 # debain ubuntu自动安装 zerotier 并设置的为planet服务器
 # addr服务器公网ip+port
 ip=`wget http://ipecho.net/plain -O - -q ; echo`
-#addr=$ip/9993
-addr=$ip
+addr=$ip/9993
 apt autoremove
 apt update -y
 apt install curl -y
@@ -19,13 +18,8 @@ echo "identity :$identity=============================================="
 apt-get -y install build-essential
 apt-get install git -y
 wget https://gitee.com/MINGERTAI/docker-zerotier-aio-zh/raw/master/planet.tar.gz && tar zxvf planet.tar.gz && chmod +x /root/planet && rm -rf planet.tar.gz && cd /root/planet/attic/world
-#sed -i "/roots.push_back/d" ./mkworld.cpp
-#sed -i "/roots.back()/d" ./mkworld.cpp 
-#sed -i "85i roots.push_back(World::Root());" ./mkworld.cpp 
-#sed -i "86i roots.back().identity = Identity(\"'"$identity"'\");" ./mkworld.cpp 
-#sed -i "87i roots.back().stableEndpoints.push_back(InetAddress(\"'"$addr"'\"));" ./mkworld.cpp
 sed -i '88s/3a46f1bf30:0:76e66fab33e28549a62ee2064d1843273c2c300ba45c3f20bef02dbad225723bb59a9bb4b13535730961aeecf5a163ace477cceb0727025b99ac14a5166a09a3/'$identity'/' ./mkworld.cpp
-sed -i '89s/185.180.13.82/'$addr'/' ./mkworld.cpp
+sed -i '89s#185.180.13.82/9993#'$addr'#g' ./mkworld.cpp
 sed -i '90s/roots.back/\/\/roots.back/' ./mkworld.cpp
 source ./build.sh
 sleep 8s
